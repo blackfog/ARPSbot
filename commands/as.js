6dc6c5841b3719952bf5b207a27d28084683e667
Craig Pearlman <craig@blackfog.net>
2020-05-11 10:02:02 -0400

    Checkpoint

--- commands/as.js
+++ commands/as.js
@@ -5,20 +5,32 @@ module.exports = {
         '/as <name> say <text|macro|var> [<language: text|macro|var>]',
         '/as <name> do <text|macro|var>'
     ],
+    showUsage(message, label = "Usage:") {
+        return message.channel.send('<@' + message.author.id + '>: ' + label + "\n" + this.usage.join("\n"));
+    },
 	execute(message, args, db) {
         if (args.length < 3) {
-            message.channel.send("*** SHOW USAGE");
+            this.showUsage(message);
+            return;
         }
 
-        const name      = args[0];
-        const operation = args[1];
+        const name      = args[0].toLowerCase();
+        const operation = args[1].toLowerCase();
         const action    = args[2];
-        const language  = args[3] === 'undefined' ? null : args[3];
+        const language  = args[3];
+
+        if (operation !== "say" && operation !== "do") {
+            this.showUsage(message, "Invalid action:");
+            return;
+        }
+
+        if (operation !== "say" && language !== undefined) {
+            this.showUsage(message, 'Language is only allowed with the say operator:');
+            return;
+        }
 
         // TODO: does the character name exist? If not, error
-        // TODO: is action either say or do? If not, error
-        // TODO: if action != say and language is not null, error
 
-		message.channel.send('NOT IMPLEMENTED YET'); // depends on /char
+		message.reply('I\'m sorry, but /as is not implemented yet'); // depends on /char
 	}
 };
